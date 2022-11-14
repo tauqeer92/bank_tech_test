913973567792bc6321ae68ea22b55a611b168f9b
Tauqeer Shoaib <tauqeershoaib@gmail.com>
2022-11-14 17:30:36 +0000

    can see a list of transactions with either credit or debit

--- lib/bank_tech_test.rb
+++ lib/bank_tech_test.rb
@@ -1,18 +1,25 @@
 class Banking
     def initialize
       @balance = 0
+      @transactions = []
     end
 
     def deposit(amount)
       @balance += amount
+      @transactions.push({credit: amount})
     end
 
     def withdraw(amount)
       @balance -= amount
+      @transactions.push({debit: amount})
     end
 
     def balance
       @balance
+    end
+
+    def transactions
+      @transactions
     end
 
     
--- spec/bank_tech_test_spec.rb
+++ spec/bank_tech_test_spec.rb
@@ -7,6 +7,27 @@ RSpec.describe Banking do
         banking.deposit(500)
         expect(banking.balance).to eq (500)
     end
+
+    it 'depositing twice, expect balance to be 1000' do
+        banking = Banking.new
+        banking.deposit(500)
+        banking.deposit(500)
+        expect(banking.balance).to eq (1000)
+    end
+
+    it 'depositing 400, expect transactions to include credit:400' do
+        banking = Banking.new
+        banking.deposit(400)
+        expect(banking.transactions).to eq ([{credit: 400}])
+    end
+
+    it 'depositing twice, expect 2 hashes in transactions with credit' do
+        banking = Banking.new
+        banking.deposit(400)
+        banking.deposit(700)
+        expect(banking.transactions).to eq ([{credit: 400}, {credit: 700}])
+    end
+
   end
 
   context 'withdrawing from bank' do
@@ -15,6 +36,13 @@ RSpec.describe Banking do
       banking.deposit(500)
       banking.withdraw(100)
       expect(banking.balance).to eq(400)
+    end
+
+    it 'depositing 400, withdrawing 200 expect transactions to include debit and credit' do
+        banking = Banking.new
+        banking.deposit(400)
+        banking.withdraw(200)
+        expect(banking.transactions).to eq ([{credit: 400}, {debit: 200}])
     end
   end
   
